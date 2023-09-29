9eee98f95fbe695b77e4f023bb090c56dd7f7ef4
KenCorp <kennethwarui35@gmail.com>
2023-09-29 17:31:51 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -18,22 +18,50 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        if username and password:
+
+            new_user = User(username=username)
+                
+            new_user.password_hash = password
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            return new_user.to_dict(), 201
+        
+        return {'error': '422 Unprocessable Entity'}, 422
 
 class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict, 200
+        else:
+            return {}, 204
     pass
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'error': '401 Not authorized'}, 401
+        
+        
 
 class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
     pass
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
